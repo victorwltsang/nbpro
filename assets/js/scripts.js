dbf22bbc6c4c7ed7fe7e899c467b0596325abb17
victorwltsang <victorwltsang@gmail.com>
2017-07-27 10:56:10 -0700

    fixed nav

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  $(".nav a").on("click", function() {
+    $(".nav").find(".active").removeClass("active");
+    $(this).parent().addClass("active");
+  });
+
+});
